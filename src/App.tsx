3541b1730855f86722c101f64aa6e838c8f22f63
Din Bibek Thapa <legendsnever7die@gmail.com>
2021-11-29 11:48:58 +0545

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import './App.css'
-import { Form, Input, Button } from 'antd'
 import { GlobalStyles } from './Global';
+import Router from './routes/Router'
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <h2>Hamro Service</h2>
-      <Form>
-        <Form.Item label='username'>
-          <Input />
-        </Form.Item>
-        <Button  type='dashed'> click me </Button>
-      </Form>
+      <Router />
     </>
   );
 }
---
+++ src/routes/Router.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from "../screens/Home"
+import Login from "../screens/Login"
+
+const Router = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/login' element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    ) 
+}
+
+export default Router
