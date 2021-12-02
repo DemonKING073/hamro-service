18a9b78f4f278955f7227703ec9c1e690f6eec09
Din Bibek Thapa <legendsnever7die@gmail.com>
2021-12-02 16:12:38 +0545

    side drawer added

--- src/features/Region.tsx
+++ src/features/Region.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import MainTemplate from "../components/MainTemplate";
-import { Menu, Dropdown, Button, Select, Input } from 'antd'
-import { FileAddOutlined, SearchOutlined, SettingOutlined } from '@ant-design/icons'
+import { Menu, Dropdown, Button, Select, Input, Drawer, Form } from 'antd'
+import { FileAddOutlined, SearchOutlined, SettingOutlined, CloseOutlined } from '@ant-design/icons'
 
 const { Option } = Select;
 
@@ -41,6 +41,11 @@ const SortButton = styled.span`
     padding: 5px 16px;
     border-radius: 10px;
 `
+const FormButton = styled(Button)`
+    width: 100% ;
+    marginTop: 18px;
+`
+
 
 const sort = (
     <Menu>
@@ -57,8 +62,59 @@ const sort = (
   );
 
 const Region = () => {
+    const [form] = Form.useForm()
+    const [ showDrawer, setShowDrawer ] = useState<any>(true)
+    const closeDrawer = () => {
+        setShowDrawer(false)
+    }
+    const onFinish = async (values:any) => {
+        console.log('Received values of form: ', values);
+      };
     return(
         <MainTemplate>
+            <Drawer closeIcon={<CloseOutlined style={{color:'white'}} />} headerStyle={{backgroundColor:'var(--primary)'}} title={<span style={{color:'white'}}>Add Region Description</span>} placement='right' onClose={closeDrawer} visible={showDrawer}>
+                <Form  form={form} layout='vertical' onFinish={onFinish}>
+                    <Form.Item 
+                        label='Name' 
+                        name='name'
+                        rules={[
+                            {
+                              required: true,
+                              message: 'Please input Region Name!',
+                            },
+                          ]}
+                    >
+                        <Input type='text' />
+                    </Form.Item>
+                    <Form.Item 
+                        label='Longitude' 
+                        name='lng'
+                        rules={[
+                            {
+                              required: true,
+                              message: 'Please input Longitude!',
+                            },
+                          ]}
+                    >
+                        <Input type='number' />
+                    </Form.Item>
+                    <Form.Item 
+                        label='Latitude' 
+                        name='lat'
+                        rules={[
+                            {
+                              required: true,
+                              message: 'Please input Longitude!',
+                            },
+                          ]}
+                    >
+                        <Input type='number' />
+                    </Form.Item>
+                    <Form.Item>
+                        <FormButton htmlType='submit' size='large' type='primary'>Submit</FormButton>
+                    </Form.Item>
+                </Form>
+            </Drawer>
             <TopContainer>
                 <Select defaultValue='alltime' style={{marginRight:20}} >
                     <Option value='alltime'>All Time</Option>
@@ -72,7 +128,7 @@ const Region = () => {
                     <Option value='regionaladmin'>Regional Admin</Option>
                     <Option value='superadmin'>Super Admin</Option>
                 </Select>
-                <Button style={{backgroundColor:'var(--primary)', color: 'white',borderRadius:6}}  ><FileAddOutlined />Add Region</Button>
+                <Button onClick={() => setShowDrawer(true)} style={{backgroundColor:'var(--primary)', color: 'white',borderRadius:6}}  ><FileAddOutlined />Add Region</Button>
             </TopContainer>
             <SettingContainer>
                 <SortContainer>
