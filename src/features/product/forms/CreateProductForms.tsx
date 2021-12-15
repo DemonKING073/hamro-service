8840d1b4a3744214fb16cc7e42e35d74b9dcb095
Din Bibek Thapa <legendsnever7die@gmail.com>
2021-12-15 14:14:32 +0545

    product add form completed

--- src/features/product/forms/CreateProductForms.tsx
+++ src/features/product/forms/CreateProductForms.tsx
@@ -9,6 +9,12 @@ import axiosCheckError from "../../../axiosCheckError";
 import NotificationService from "../../../services/NotificationService";
 import RegionProps from '../../../types/Region'
 import CategoryProp from '../../../types/Category'
+import BaseProductProp from '../../../types/BaseProduct'
+import VendorProps from '../../../types/Vendor'
+import { getBaseProducts } from "../../../apis/baseProduct";
+import axios from "axios";
+import ApiError from "../../../types/ApiError";
+import { GetVendor } from "../../../apis/vendor";
 
 
 type CustomDrawerProps = DrawerProps & {
@@ -21,13 +27,99 @@ const showValues = (val: any) => {
     console.log('yo values hoo',val)
 } 
 
+
+
 const { Option } = Select;
 const CreateProductForms:FC<CustomDrawerProps> = ({onFinish, visible, onClose}) => {
+    const [ workingBaseProduct, setWorkingBaseProduct ] = useState<BaseProductProp>()
     const [form] = Form.useForm()
+    const [ currentRegion, setCurrentRegion ] = useState<RegionProps>()
+    const [ currentVendor, setCurrentVendor ] = useState<VendorProps>()
+    const { data: regionData } = useQuery('fetchRegion',getRegion,{
+        onSuccess: (regionData) => {
+            if(regionData) setCurrentRegion(regionData[0])
+        },
+        onError: (err) => {
+            const apiError = axiosCheckError(err)
+            if(apiError && apiError.message) NotificationService.showNotification('error', apiError.message.toString())
+        }
+    })
+    const {data: baseProduct, isLoading: isBaseProductLoading, refetch: refetchBaseProducts } = useQuery(['fetchBaseProduct', currentRegion],() => {
+        if(currentRegion) return getBaseProducts(currentRegion.id)
+    }, {
+        onError: (err) => {
+            if(axios.isAxiosError(err)) {
+                const apiError = err as ApiError
+                const message = apiError.response?.data.message
+                if (message) NotificationService.showNotification('error', message.toString())
+            }
+        },
+        onSuccess: (data) => {
+            if(data) setWorkingBaseProduct(data[0])
+        }
+    })
+    const { data: vendorDatas, refetch: refetchVendorData } = useQuery(['fetchVendor', currentRegion], () => {
+        if(currentRegion) return GetVendor(currentRegion.id)
+    },{
+        onError: (err) => {
+            if(axios.isAxiosError(err)) {
+                const apiError = err as ApiError
+                const message = apiError.response?.data.message
+                if (message) NotificationService.showNotification('error', message.toString())
+            }
+        },
+        onSuccess: (data: VendorProps[]) => {
+            console.log(data)
+            if(data) setCurrentVendor(data[0])
+        }
+    })
+    
+    const handleRegionChange = (value: any) => {
+        const newRegion = regionData?.find((item) => item.id === value)
+        setCurrentRegion(newRegion)
+    }
+    const handleBaseProductChange = (value: any) => {
+        const newBaseProduct = baseProduct?.find((item) => item.id === value)
+        setWorkingBaseProduct(newBaseProduct)
+    }
+    useEffect(() => {
+        form.resetFields()
+    },[workingBaseProduct, form])
+
 
     return(
         <Drawer visible={visible} onClose={onClose}  closeIcon={<CloseOutlined style={{color:'white'}} />} headerStyle={{backgroundColor:'var(--primary)'}} title={<span style={{color:'white'}}>Add Product</span>} placement='right'>
-            <Form initialValues={{featured: false,}} form={form} layout='vertical' onFinish={showValues}>
+            <Form initialValues={{featured: false,regionId: currentRegion?.id, baseProduct: workingBaseProduct?.name, vendorId: currentVendor?.id}} form={form} layout='vertical' onFinish={showValues}>
+                    <Form.Item
+                        label='Region'
+                        name='regionId'
+                    >
+                        <Select value={currentRegion?.name} onChange={handleRegionChange}>
+                            {regionData?.map((item) => (
+                                <Option value={item.id} key={item.id} >{item.name}</Option>
+                            ))}
+                        </Select>
+                    </Form.Item>
+                    <Form.Item
+                        label='BaseProduct'
+                        name='baseProduct'
+                    >
+                        <Select value={workingBaseProduct?.name} onChange={handleBaseProductChange}>
+                            {baseProduct?.map((item) => (
+                                <Option value={item.id} key={item.id} >{item.name}</Option>
+                            ))}
+                        </Select>
+                    </Form.Item>
+                    <Form.Item
+                        label='Vendor'
+                        name='vendorId'
+                    >
+                        <Select value={currentVendor?.name} onChange={handleBaseProductChange}>
+                            {vendorDatas?.map((item) => (
+                                <Option value={item.id} key={item.id} >{item.name}</Option>
+                            ))}
+                        </Select>
+                    </Form.Item>
                     <Form.Item 
                         label='Name' 
                         name='name'
